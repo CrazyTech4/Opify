04aca616e1ac0990a136aa2314d03624dd1adcdd
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2020-09-08 21:13:20 +0200

    fixed update and small adjustments

--- src/repository.ts
+++ src/repository.ts
@@ -5,22 +5,55 @@ interface EntityData {
 }
 
 export class Entity<Type extends EntityData> {
-    private data: Partial<Type> = {};
+    public data: Partial<Type> = {};
 
     constructor(private tablename: string) { }
+
+    /**
+     * turns values that are entities into their id
+     */
+    private justifiedData() {
+        const justifiedData = Object.assign({}, this.data);
+        Object.keys(justifiedData)
+            .forEach(key => {
+                if (key === 'id') {
+                    delete justifiedData[key];
+                    return;
+                }
+
+                const entity = justifiedData[key];
+                if (entity instanceof Entity) {
+                    entity.merge(); // for entities that aren't uptodate with the database
+                    delete justifiedData[key];
+                    justifiedData[key + '_id'] = entity.data.id;
+                    return;
+                }
+            });
+        return justifiedData;
+    }
 
     set(data: Partial<Type>) {
         Object.assign(this.data, data);
     }
 
     async insert() {
-        const keys = Object.keys(this.data);
-        const values = Object.values(this.data);
-        const [result, buff] = await Database.execute(`INSERT INTO ${this.tablename} ? VALUES ?`, [keys, values]) as any;
+        const data = this.justifiedData();
+        const keys = Object.keys(data).join(',');
+        const values = Object.values(data);
+        const [result, buff] = await Database.query(`INSERT INTO ${this.tablename} (${keys}) VALUES (?)`, [values]) as any;
         this.data.id = result.insertId;
     }
 
     async update() {
-        await Database.execute(`UPDATE ${this.tablename} WHERE id=? SET ?`, [this.data.id, this.data]);
+        const data = this.justifiedData();
+        await Database.query(`UPDATE ${this.tablename} SET ? WHERE id=?`, [data, this.data.id]);
+    }
+
+    async merge() {
+        if (this.data.id) {
+            await this.update();
+        } else {
+            await this.insert();
+        }
     }
 }
